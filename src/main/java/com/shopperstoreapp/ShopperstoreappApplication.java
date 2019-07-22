ac7ab0f44538c80d1c343c130bf3f214717da6ca
Saikat Majumder <saikatm.net@gmail.com>
2019-07-22 17:33:59 +0530

    Initial Project setup
    Initial project setup code

---
+++ src/main/java/com/shopperstoreapp/ShopperstoreappApplication.java
@@ -0,0 +1,13 @@
+package com.shopperstoreapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopperstoreappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopperstoreappApplication.class, args);
+	}
+
+}
